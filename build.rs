cb96a13570ccce62fe9642c6bf91859e1b0f74ef
Michael Fairley <michaelfairley@gmail.com>
2017-07-23 02:50:32 -0500

    aarch64-apple-ios

--- build.rs
+++ build.rs
@@ -160,6 +160,25 @@ fn main() {
 
       println!("cargo:rustc-link-search=native={}", artifact_dir);
     },
+    "aarch64-apple-ios" => {
+      let artifact_dir = format!("{}/build", out_dir);
+      {
+        let proj_dir = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("SDL2-2.0.5/Xcode-iOS/SDL/SDL.xcodeproj");
+
+        let mut cmd = Command::new("xcodebuild");
+        cmd
+          .current_dir(&out_dir)
+          .args(&["-configuration", "Release"])
+          .args(&["-arch", "arm64"])
+          .args(&["-sdk", "iphoneos"])
+          .args(&["-target", "libSDL"])
+          .args(&["-project", proj_dir.to_str().unwrap()])
+          .arg(format!("CONFIGURATION_BUILD_DIR={}", artifact_dir));
+        run(&mut cmd);
+      }
+
+      println!("cargo:rustc-link-search=native={}", artifact_dir);
+    },
     other => panic!("sdl2-lib is not implemented for {}", other),
   }
 }
